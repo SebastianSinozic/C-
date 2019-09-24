d34c5da14a140c47c5d6b7e185b70d67f7753dd4
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2019-09-24 12:13:14 +0200

    Add files via upload

---
+++ sinozicSebastianMojProjekt2/sinozicSebastianMojProjekt2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinozicSebastianMojProjekt2
+{
+    public partial class Form1 : Form
+    {
+        int broj = 0;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            broj++;
+            txtPrikaz.Text = broj.ToString();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            broj--;
+            txtPrikaz.Text = broj.ToString();
+        }
+    }
+}
